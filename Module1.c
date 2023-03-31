6e8c6865f9568550287fa5e5a33ecac08b71310b
Deepvarasada <128610480+Deepvarasada@users.noreply.github.com>
2023-03-31 21:53:48 +0530

    Add files via upload

---
+++ Module1.c
@@ -0,0 +1,8 @@
+//Display This Information using printf
+
+#include<stdio.h>
+
+void main()
+{
+	printf("My Name Is Deep.\nI Am Born In 11 Jan 2003.\nI Am 20 Year Old.\nI Am Live In Vijaynagar-3 ,Ravapar Road,Morbi-363641");
+}
